85507535d2f7c8a63ea5b85f4b76bd648f576db5
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2023-03-29 11:11:59 +0200

    Fixed padding on mobile

--- src/routes/[lang]/[job]/+page.svelte
+++ src/routes/[lang]/[job]/+page.svelte
@@ -6,7 +6,7 @@
 
 <a href="/{data.lang}" class="absolute left-4 top-4 hover:underline">{data.static.back}</a>
 
-<div class="flex p-16 justify-center">
+<div class="flex py-16 px-8 justify-center">
 	<div class="md:w-2/3 lg:w-1/2">
 		<h1 class="text-2xl">{data.job.title}</h1>
 
